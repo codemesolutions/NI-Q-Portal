ab29a8fcb4280df99a21d071fea6cb4c002e0dc8
Joshua Getner <jgetner@codeme.solutions>
2019-11-13 14:39:24 -0800

    update thank you pages for mobile

--- system/resources/views/site/disqualify.blade.php
+++ system/resources/views/site/disqualify.blade.php
@@ -2,7 +2,7 @@
 
 
 @section('content')
-    
+
 <div class="jumbotron jumbotron-fluid bg-teal  border-top">
     <div class="container py-3">
         <h6 class="m-0 text-white">{!!$title!!}</h6>
@@ -10,10 +10,10 @@
 </div>
 
 <div class="bg-white">
-    <div class="container p-5">
-        <div class=" p-5">
-           
-            <div class="w-75 mx-auto answer border bg-white p-5">
+    <div class="container py-5 p-md-5">
+        <div class=" p-md-5">
+
+            <div class="w-75 mx-auto answer border bg-white p-3 p-md-5">
                <h6 class="font-weight-bold mb-3  bg-light p-3 ">{{$disqualified_title}}</h6>
                <p>{{$disqualified_message}}</p>
                 <a class="btn btn-teal mt-3" href="http://ni-q.com">Return to NI-Q</a>
--- system/resources/views/site/thankyou.blade.php
+++ system/resources/views/site/thankyou.blade.php
@@ -2,7 +2,7 @@
 
 
 @section('content')
-    
+
 <div class=" jumbotron jumbotron-fluid bg-image py-5">
     <div class="container py-5 text-left">
         <div class="py-5 text-white">
@@ -12,10 +12,10 @@
 </div>
 
 <div class="bg-white">
-    <div class="container p-5">
-        <div class=" p-5">
-           
-          <div class="w-75 mx-auto answer border bg-white p-5">
+    <div class="container py-5 p-md-5">
+        <div class=" p-md-5">
+
+          <div class="w-75 mx-auto answer border bg-white p-3 p-md-5">
                <h6 class="font-weight-bold mb-3  bg-light p-3 ">{{$ty_title}}</h6>
                <p>{{$ty_message}}</p>
                 <a class="btn btn-teal mt-3" href="http://ni-q.com">Return to NI-Q</a>
